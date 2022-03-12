e95fd4ce9895e25b2e9cf06cb4d19725546789f1
debug-anywhere001 <i@debug-anywhere.com>
2022-03-12 17:21:49 +0800

    new version

---
+++ _next/static/chunks/pages/en/devtools/thanks-c271debb6499f3c5.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[414],{3961:function(n,_,u){(window.__NEXT_P=window.__NEXT_P||[]).push(["/en/devtools/thanks",function(){return u(4128)}])}},function(n){n.O(0,[128,774,888,179],(function(){return _=3961,n(n.s=_);var _}));var _=n.O();_N_E=_}]);
---
+++ _next/static/chunks/pages/en/devtools/thanks.txt-5246d0d37b694c05.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[539],{353:function(n,t,u){(window.__NEXT_P=window.__NEXT_P||[]).push(["/en/devtools/thanks.txt",function(){return u(5205)}])},5205:function(n,t,u){"use strict";u.r(t);var _=u(4128);t.default=_.default}},function(n){n.O(0,[128,774,888,179],(function(){return t=353,n(n.s=t);var t}));var t=n.O();_N_E=t}]);
