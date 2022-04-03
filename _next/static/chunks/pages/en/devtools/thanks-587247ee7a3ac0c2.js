15dde06b2ebfd183c8f29087f91cefad92428535
debug-anywhere001 <i@debug-anywhere.com>
2022-04-04 00:13:48 +0800

    feat(tools): 增加tools

---
+++ _next/static/chunks/pages/en/devtools/thanks-587247ee7a3ac0c2.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[9414],{3961:function(n,_,u){(window.__NEXT_P=window.__NEXT_P||[]).push(["/en/devtools/thanks",function(){return u(9945)}])}},function(n){n.O(0,[9945,9774,2888,179],(function(){return _=3961,n(n.s=_);var _}));var _=n.O();_N_E=_}]);
---
+++ _next/static/chunks/pages/en/devtools/thanks.txt-1b0f175d0e4bd0d2.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[7539],{353:function(n,t,u){(window.__NEXT_P=window.__NEXT_P||[]).push(["/en/devtools/thanks.txt",function(){return u(2691)}])},2691:function(n,t,u){"use strict";u.r(t);var _=u(9945);t.default=_.default}},function(n){n.O(0,[9945,9774,2888,179],(function(){return t=353,n(n.s=t);var t}));var t=n.O();_N_E=t}]);
