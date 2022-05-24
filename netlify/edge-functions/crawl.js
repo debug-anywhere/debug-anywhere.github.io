4d5e90ec046935c638dc86ac08906fa3957a6e11
debug-anywhere001 <i@debug-anywhere.com>
2022-05-24 12:01:46 +0800

    feat(open-graph): crawl api

---
+++ netlify/edge-functions/crawl.js
@@ -0,0 +1,36 @@
+export default async (req, context) => {
+	try {
+		const search = new URLPattern(req.url).search || '';
+		const searchArr = search.split('&').filter(it => it.startsWith('url='));
+		if (!searchArr.length) {
+			throw new Error('invalid url');
+		}
+		const searchUrl = searchArr[0].split('=')[1] || '';
+
+		const html = await fetch(decodeURIComponent(searchUrl), {
+			'headers': {
+				'Content-Type': 'text/plain',
+				'User-Agent': req.headers['user-agent'] || ''
+			}
+		}).then(res => {
+			return res.text();
+		}).then((data) => {
+			return data;
+		});
+		return new Response(JSON.stringify({ code: 0, html }), {
+			headers: {
+				'content-type': 'application/json',
+				'Access-Control-Allow-Origin': 'www.debug-anywhere.com'
+			}
+		})
+		// return context.json({ code: 0, html: html });
+	} catch (e) {
+		return new Response(JSON.stringify({ code: 1, msg: e.message }), {
+			headers: {
+				'content-type': 'application/json',
+				'Access-Control-Allow-Origin': 'www.debug-anywhere.com'
+			}
+		})
+		// return context.json({ code: 1, msg: e.message });
+	}
+}
