de71587d7683267bc9f85459ee9f4bae568ee8bc
debug-anywhere001 <i@debug-anywhere.com>
2024-11-16 23:18:06 +0800

    update edge function

--- netlify/edge-functions/log.js
+++ netlify/edge-functions/log.js
@@ -1,4 +1,4 @@
-import { createClient } from "@supabase/supabase-js";
+import { createClient } from "npm:@supabase/supabase-js@2";
 
 const supabase = createClient(
   process.env.SUPABASE_DATABASE_URL,
